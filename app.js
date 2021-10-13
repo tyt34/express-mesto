307fa506be4d3490983d75657f5403ba0c2158ec
R N <novoto4in@mail.ru>
2021-10-13 21:11:38 +0300

    0.06

--- app.js
+++ app.js
@@ -9,9 +9,9 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 mongoose.connect('mongodb://localhost:27017/mestodb', {
-  // useNewUrlParser: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 app.use((req, res, next) => {
